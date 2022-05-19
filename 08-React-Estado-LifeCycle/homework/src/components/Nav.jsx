7c4b6b504db98c0ff8701ee15bda7ad80c7aa1bb
MarianBzz <mbozzoletti@hotmail.com>
2022-05-18 21:22:18 -0300

    Homework React State

--- 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
@@ -5,8 +5,18 @@ import './Nav.css';
 
 function Nav({onSearch}) {
   return (
-    <div>Nav</div>
+    <div className='barranav'>
+      <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+        <img src={Logo} width="30" height="30" className="d-inline-block align-top" alt="Logo Henry" />
+         Henry - Weather App
+      </a>
+      <SearchBar onSearch={onSearch} />
+      </nav>
+    </div>
+
   );
 };
+
 
 export default Nav;
